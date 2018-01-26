ca96b29450ee694f83d900e84735c9daff95f5e0
Randy Merrill <randy@blinkk.com>
2018-01-26 11:14:57 -0700

    Adding initial tests for the extracted messages class.

--- grow/translations/extractor.py
+++ grow/translations/extractor.py
@@ -1,19 +1,90 @@
 """Grow translation extractor."""
 
-from babel.messages import pofile
+import re
 
 
 class Error(Exception):
+    """Base error for extracting."""
+    pass
+
+
+class MissingBaseError(Error):
+    """Error with missing a base string for a translation."""
     pass
 
 
 class Extractor(object):
+    """Translation extractor for finding translatable strings.
+
+    Also supports extracting already translated strings from objects with tagging.
+
+    ```
+    foo@: "Extracted for translation."
+    foo@es: "Not extracted, tagging overwrites value in document fields."
+    foo@es@: "Extracted as the translation for foo@, tagging keeps the foo@ value."
+    ```
+    """
 
     def __init__(self, pod):
         self.pod = pod
+        self.results = ExtractedMessages()
 
-    def extract_object(self, obj):
+    def extract_object(self, obj, source=None):
+        """Extract the translatable strings from an object.
+
+        Translateable strings are tagged with an ending @ symbol.
+
+        Keys can be tagged with a locale and a trailing @ (ex: foo@es@) to be extracted as a
+        translation of the base string (foo@).
+        """
         pass
+
+    def extract_template(self, template):
+        """Extract the translatable strings from a template."""
+        pass
+
+
+class ExtractedMessages(object):
+    """Results from the extraction process to retrieve extracted strings with locale support."""
+
+    def __init__(self):
+        self.meesages_to_locations = {}
+        self.pattern_to_messages = {}
+
+    @property
+    def messages(self):
+        """Messages that have been extracted."""
+        return self.meesages_to_locations.keys()
+
+    def add_message(self, message, location=None):
+        """Add a message to the results."""
+        if message not in self.meesages_to_locations:
+            self.meesages_to_locations[message] = set()
+        if location:
+            self.meesages_to_locations[message].add(location)
+
+    def add_translation(self, pattern, message, translated):
+        """Add a translation from the extraction."""
+        if not message:
+            raise MissingBaseError(u'Missing base string for translation: {}'.format(translated))
+
+        if pattern not in self.pattern_to_messages:
+            self.pattern_to_messages[pattern] = {}
+        self.pattern_to_messages[pattern][message] = translated
+
+    def get_translations(self, locale):
+        """Retrieve the translations that match a given locale."""
+        translations = {}
+        locale = str(locale)
+
+        # Only return the translations that match the locale pattern to the locale.
+        for locale_pattern, messages in self.pattern_to_messages.iteritems():
+            locale_re = re.compile(locale_pattern)
+            if locale_re.search(locale):
+                for message, translation in messages.iteritems():
+                    translations[message] = translation
+
+        return translations
 
 
 # Ideas
--- grow/translations/extractor_test.py
+++ grow/translations/extractor_test.py
@@ -4,16 +4,63 @@ import unittest
 from grow.pods import pods
 from grow import storage
 from grow.testing import testing
+from grow.translations import extractor
 
 
 class ExtractorTest(unittest.TestCase):
+    """Translation extractor tests."""
 
     def setUp(self):
         dir_path = testing.create_test_pod_dir()
         self.pod = pods.Pod(dir_path, storage=storage.FileStorage)
+        self.extract = extractor.Extractor(self.pod)
 
     def test_object(self):
+        """?"""
         pass
+
+
+class ExtractedMessagesTest(unittest.TestCase):
+    """Extracted messages tests."""
+
+    def setUp(self):
+        self.results = extractor.ExtractedMessages()
+
+    def test_message(self):
+        """Messages are part of the extracted string results."""
+        self.results.add_message('foo')
+        self.assertEqual(set(['foo']), set(self.results.messages))
+
+    def test_translations(self):
+        """Translations are part of the extracted results."""
+        self.results.add_translation('es', 'foo', 'foobar')
+        self.assertEqual({
+            'foo': 'foobar',
+        }, self.results.get_translations('es'))
+
+    def test_translations_patterns(self):
+        """Patterns are used to match the locales against translations."""
+        self.results.add_translation('(es|fr)', 'foo', 'foobar')
+        self.results.add_translation('es', 'fuz', 'ball')
+        self.results.add_translation('fr', 'bar', 'bell')
+        self.assertEqual({
+            'foo': 'foobar',
+            'fuz': 'ball',
+        }, self.results.get_translations('es'))
+        self.assertEqual({
+            'foo': 'foobar',
+            'bar': 'bell',
+        }, self.results.get_translations('fr'))
+        self.assertEqual({}, self.results.get_translations('en'))
+
+    def test_missing_base(self):
+        """Translations cannot be missing the base string."""
+        with self.assertRaises(extractor.MissingBaseError):
+            self.results.add_translation('es', '', 'foobar')
+
+        with self.assertRaises(extractor.MissingBaseError):
+            self.results.add_translation('es', None, 'foobar')
+
 
 if __name__ == '__main__':
     unittest.main()
